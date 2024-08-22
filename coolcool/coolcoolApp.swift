9a440356d69c77ff84f6a9c16c629c0699b75953
kimtaeyoung <>
2024-08-22 23:32:21 +0900

    - project init

---
+++ coolcool/coolcoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  coolcoolApp.swift
+//  coolcool
+//
+//  Created by taeyoung on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct coolcoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
